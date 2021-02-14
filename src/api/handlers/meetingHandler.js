8df18ca1f911a200cb5d2ae5fdfbdd170ba2ed50
jaditya8109 <jaditya8109@gmail.com>
2021-02-14 19:20:38 +0530

    validation of input fields done

--- src/api/handlers/meetingHandler.js
+++ src/api/handlers/meetingHandler.js
@@ -13,10 +13,15 @@ async function saveRoomAndCreatorAndStartMeet (req,res){
                 meetingStartedBy :  name,
                 status : "active"
                 });
-    meet.save()
-    
-    var hashedRoomName = Buffer.from(roomname).toString('base64');
-    res.status(201).send( {roomValue: hashedRoomName, userid: userID, name:name});
+    meet.save(function(err){
+            if (err){
+                res.send(err);
+            }
+            else{
+                var hashedRoomName = Buffer.from(roomname).toString('base64');
+                res.status(201).send( {roomValue: hashedRoomName, userid: userID, name:name});
+                }
+    })
 }
 
 async function changeMeetStatus (req,res){
@@ -25,9 +30,14 @@ async function changeMeetStatus (req,res){
     //update status
     let meetingData = await  meetingModel.findOne({roomName:originalText})
     meetingData.status = "inactive"
-    await meetingData.save();
-
-    res.send("meeting status of room name = " + originalText + " and hashed room name = " + req.body.remove + " changed to inactive");
+    meetingData.save(function(err){
+        if (err){
+            res.send(err);
+        }else{
+            // res.send("meeting status of room name = " + originalText + " and hashed room name = " + req.body.remove + " changed to inactive");
+            res.send({"message":"status chnaged to inactive"});
+        }
+    });  
 }
 
 function activeMeetDetails (req,res){
@@ -48,10 +58,14 @@ async function joinMeetForParticipants(req,res){
             $push : {participants : {joinee : ParticipantName}}
         })
         .catch(err => console.log(err));  ;
-        meetingData.save();
-        
-        var hashedRoomName =Buffer.from(roomname).toString('base64');
-        res.send({roomValue: hashedRoomName, joinedBy: ParticipantName});
+        meetingData.save({new:true, upsert: true, timestamps:{createdAt:false, updatedAt:true}} ,function(err){
+            if (err){
+                res.send(err);
+            }else{
+                var hashedRoomName =Buffer.from(roomname).toString('base64');
+                res.send({roomValue: hashedRoomName, joinedBy: ParticipantName});
+            }
+        });
         }  catch(err){
             console.error(err)
             res.status(500).send(err.message)
@@ -74,8 +88,15 @@ async function postmom(req,res){
         $set : {MOM : mom}
     })
     .catch(err => console.log(err));  ;
-    await meetingData.save();
-    res.send("mom saved!")
+    meetingData.save({new:true, upsert: true, timestamps:{createdAt:false, updatedAt:true}} ,function(err){
+        if (err){
+            res.send(err);
+        }else{
+            // res.send("mom saved!");
+            res.send({"message" : "mom saved!" });
+        }
+    });
+    
 }
 
 
